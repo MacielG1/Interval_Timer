6a39fa70cc66cf3cc4b821847a0ed6a8d40fffc2
Gustavo Maciel <guflumaciel@gmail.com>
2023-01-05 11:39:07 -0300

    added local storage to save timers

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let inputs = document.querySelectorAll(".options input");
 //display data
-let mainTimeDisplay = document.querySelector(".display .timer");
+let timeDisplay = document.querySelector(".display .timer");
 let roundsDisplay = document.querySelector(".display .roundsDisplay");
 // progress bar
 let progressBar = document.querySelector("#progressBar");
@@ -21,228 +21,69 @@ let restColor = document.querySelector("#restColor");
 // timerInputs
 let timeInput = document.querySelectorAll(".timeInput");
 //buttons inputs
-let startBtn = document.querySelector(".buttons .start");
-let stopBtn = document.querySelector(".buttons .stop");
-let resetBtn = document.querySelector(".buttons .reset");
+
+const startBtn = document.querySelector(".buttons .start");
+const pauseBtn = document.querySelector(".buttons .stop");
+const resetBtn = document.querySelector(".buttons .reset");
+
 let saveWorkoutBtn = document.querySelector(".saveWorkoutBtn");
-// sidebar Buttons
-let startWorkoutBtn = document.querySelector(".workout .startWorkout");
-let deleteWorkoutBtn = document.querySelector(".workout .deleteBtn");
+// sidebar
+let sidebar = document.querySelector(".sidebar");
+let startWorkoutBtn;
+let deleteWorkoutBtn;
 //
 
-let counter = 0;
-let currentMode;
-let currentRound = 0;
-let counter2 = 5;
-let interval;
-let initialInt;
-let intervalId;
-let prepInterval;
-let currentSet;
-let isRunning = false;
-let whichMode;
-let currentInterval;
+let WorkoutsSaved = JSON.parse(localStorage.getItem("workouts"));
 
-timeInput.forEach((input) => {
-  input.value = input.value || "00";
-});
-// (timeWhenStoped,modeWhenStoped,roundWhenStoped);
-function createTimer(mode = "prepare", currRound = 1, time) {
-  let workTimeCurrent = [workMin.value, workSec.value];
-  let restTimeCurrent = [restMin.value, restSec.value];
-  let prepTimeCurrent = [prepareMin.value, prepareSec.value];
+if (WorkoutsSaved) {
+  showSavedWorkouts();
+}
+function showSavedWorkouts() {
+  WorkoutsSaved.forEach((workout, index) => {
+    let html = `
+  <div class="workout" data-index=${index}>
+  <button id="delete-btn" class="deleteBtn"></button>
+  <p class="title">Workout 1</p>
 
-  let workDisplay = (mainTimeDisplay.textContent = `${workTimeCurrent[0]}:${workTimeCurrent[1]}`);
-  let restDisplay = (mainTimeDisplay.textContent = `${restTimeCurrent[0]}:${restTimeCurrent[1]}`);
-  let prepDisplay = (mainTimeDisplay.textContent = `${prepTimeCurrent[0]}:${prepTimeCurrent[1]}`);
+  <div class="data">
+    <p>Rounds : ${workout.roundsDisplay}</p>
+    <p>Work: ${workout.workDisplay}</p>
+    <p>Prepare: ${workout.prepDisplay}</p>
+    <p>Rest: ${workout.restDisplay}</p>
+  </div>
+  <div class="startWorkout">
+    <button>Start</button>
+  </div>
+  </div>
+  `;
+    sidebar.insertAdjacentHTML("afterbegin", html);
+  });
+  deleteWorkoutBtn = document.querySelectorAll(".sidebar .deleteBtn");
+  deleteWorkoutBtn.forEach((deleletbtn) => {
+    deleletbtn.addEventListener("click", (event) => {
+      let index = event.target.parentElement.dataset.index;
+      WorkoutsSaved.splice(index, 1);
+      localStorage.setItem("workouts", JSON.stringify(WorkoutsSaved));
+      event.target.parentElement.remove();
+    });
+  });
+  startWorkoutBtn = document.querySelectorAll(".sidebar .startWorkout");
+  startWorkoutBtn.forEach((startbtn) => {
+    startbtn.addEventListener("click", (event) => {
+      let index = event.target.parentElement.parentElement.dataset.index;
+      let workout = WorkoutsSaved[index];
 
-  let workSecAmount = convertHour_Min_Sec_toSec(workDisplay) + 1;
-  let restSecAmount = convertHour_Min_Sec_toSec(restDisplay) + 1;
-  let prepSecAmount = convertHour_Min_Sec_toSec(prepDisplay);
-  let round;
-  let sets = rounds.value;
-  let workTime = workSecAmount;
-  let restTime = restSecAmount;
-  isRunning = false;
-  currentSet = currRound;
-  whichMode = mode;
-  initialInt = new Timer(0, function () {
-    isRunning = true;
-    round = 0;
-    // mainTimeDisplay.textContent = secondsToMMSS(prepSecAmount);
-    initialInt.stop();
-    prepInterval = new Timer(1000, function () {
-      currentInterval = prepInterval;
-      if (whichMode == "prepare") {
-        prepSecAmount--;
-        mainTimeDisplay.textContent = secondsToMMSS(prepSecAmount);
-      }
-      if (prepSecAmount == 0) {
-        prepInterval.stop();
-        whichMode = "work";
-        intervalId = new Timer(1000, function () {
-          currentInterval = intervalId;
-          if (whichMode == "work") {
-            workTime--;
-            document.body.style.backgroundColor = workColor.value;
-            progressBar.value = workTime;
-            progressBar.max = workSecAmount - 1;
-            progressBar.style.setProperty("--progressBar-color", `${workColor.value}`);
-            mainTimeDisplay.textContent = secondsToMMSS(workTime);
-          }
-          if (workTime < 0) {
-            whichMode = "rest";
-          }
-          if (whichMode == "rest") {
-            restTime--;
-            document.body.style.backgroundColor = restColor.value;
-            progressBar.value = restTime;
-            progressBar.max = restSecAmount - 1;
-            progressBar.style.setProperty("--progressBar-color", `${restColor.value}`);
-            mainTimeDisplay.textContent = secondsToMMSS(restTime);
-          }
-
-          if (restTime == 0) {
-            whichMode = "work";
-            currentSet++;
-            roundsDisplay.textContent = `${currentSet - 1}/${rounds.value}`;
-            workTime = workSecAmount;
-            restTime = restSecAmount;
-          }
-
-          if (currentSet > sets) {
-            document.body.style.backgroundColor = "black";
-            intervalId.stop();
-          }
-        });
-      }
+      rounds.value = workout.roundsDisplay;
+      prepareMin.value = workout.prepDisplay.split(":")[0];
+      prepareSec.value = workout.prepDisplay.split(":")[1];
+      workMin.value = workout.workDisplay.split(":")[0];
+      workSec.value = workout.workDisplay.split(":")[1];
+      restMin.value = workout.restDisplay.split(":")[0];
+      restSec.value = workout.restDisplay.split(":")[1];
     });
   });
 }
-// button listeners
-startBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  roundsDisplay.textContent = `${currentRound}/${rounds.value}`;
-  createTimer();
-});
-let stopCounter = 0;
-let timeWhenStoped;
-let modeWhenStoped;
-let roundWhenStoped;
-
-stopBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  timeWhenStoped = convertHour_Min_Sec_toSec(mainTimeDisplay.textContent);
-
-  modeWhenStoped = whichMode;
-  roundWhenStoped = currentSet;
-  currentInterval.stop();
-  isRunning = false;
-});
-stopBtn.addEventListener("keydown", function (e) {
-  e.preventDefault();
-  createTimer(modeWhenStoped, roundWhenStoped, timeWhenStoped);
-});
-
-saveWorkoutBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-});
-resetBtn.addEventListener("click", function () {
-  prepInterval.stop();
-  intervalId.stop();
-  mainTimeDisplay.textContent = "00:00";
-  roundsDisplay.textContent = "0/0";
-  document.body.style.backgroundColor = "black";
-  progressBar.style.setProperty("--progressBar-color", `#999999`);
-});
-
-timeInput.forEach((input) => {
-  input.addEventListener("change", function () {
-    if (this.value < 10) {
-      console.log(this.value);
-      this.value = Number(this.value).toString().padStart(2, "0");
-    }
-  });
-
-  let previousValue = "";
-  input.addEventListener("focus", function () {
-    previousValue = this.value;
-    this.value = "";
-  });
-
-  input.addEventListener("blur", function (e) {
-    if (this.value === "") {
-      this.value = previousValue;
-    }
-    if (this.value < 10) {
-      console.log(this.value);
-      this.value = Number(this.value).toString().padStart(2, "0");
-    }
-    // if (e.target.value >= 10 && e.target.value <= 60) {
-    //   e.target.value = Number(e.target.value);
-    // }
-    // if (e.target.value.length > 2) {
-    //   e.target.value = e.target.value.slice(0, 2);
-    // }
-  });
-});
-
-function convertHour_Min_Sec_toSec(str) {
-  let arr = str.split(":");
-  let seconds = Number(arr[0] * 60 + +arr[1]);
-
-  return seconds;
-}
-function secondsToMMSS(sec) {
-  const min = Math.floor(sec / 60);
-  const formattedMinutes = min < 10 ? `0${min}` : min;
-  const formattedSeconds = sec % 60 < 10 ? `0${sec % 60}` : sec % 60;
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-// class Timer {
-//   static interval;
-//   static expected;
-//   static timeout;
-//   static workFunc;
-//   static errorFunc;
-
-//   constructor(interval, workFunc, errorFunc) {
-//     this.interval = interval;
-//     this.expected = 0;
-//     this.timeout = null;
-//     this.stopped = false;
-//     this.workFunc = workFunc;
-//     this.errorFunc = errorFunc;
-//     this.start();
-//   }
-
-//   start() {
-//     this.expected = Date.now() + this.interval;
-//     this.timeout = setTimeout(this.step.bind(this), this.interval);
-//   }
-
-//   stop() {
-//     this.stopped = true;
-
-//     clearTimeout(this.timeout);
-//   }
-
-//   step() {
-//     let drift = Date.now() - this.expected;
-//     if (drift > this.interval) {
-//       console.log("Drift Higher");
-//       if (this.errorFunc) this.errorFunc();
-//     }
-//     if (!this.stopped) {
-//       this.workFunc();
-//       console.log(drift, this.expected, Math.max(0, this.interval - drift));
-//       this.expected += this.interval;
-//       this.timeout = setTimeout(this.step.bind(this), Math.max(0, this.interval - drift));
-//     }
-//   }
-// }
-
+//
 class Timer {
   static interval;
   static expected;
@@ -267,7 +108,6 @@ class Timer {
 
   stop() {
     this.stopped = true;
-
     clearTimeout(this.timeout);
   }
 
@@ -283,4 +123,184 @@ class Timer {
       this.timeout = setTimeout(this.step.bind(this), Math.max(0, this.interval - drift));
     }
   }
+}
+timeInput.forEach((input) => {
+  input.addEventListener("change", function () {
+    if (this.value < 10) {
+      this.value = Number(this.value).toString().padStart(2, "0");
+    }
+    console.log(this.value);
+  });
+
+  let previousValue = "";
+  input.addEventListener("focus", function () {
+    previousValue = this.value;
+    this.value = "";
+  });
+
+  input.addEventListener("blur", function (e) {
+    if (this.value === "") {
+      this.value = previousValue;
+    }
+    if (this.value < 10) {
+      this.value = Number(this.value).toString().padStart(2, "0");
+    }
+    // if (e.target.value >= 10 && e.target.value <= 60) {
+    //   e.target.value = Number(e.target.value);
+    // }
+    // if (e.target.value.length > 2) {
+    //   e.target.value = e.target.value.slice(0, 2);
+    // }
+  });
+});
+//
+
+let paused = true;
+let intervalId;
+let whichInterval;
+let mins = 0;
+let secs = 0;
+let mili = 0;
+let intervalWhenPaused;
+let prepDisplay = `00:05`;
+let workDisplay = `00:10`;
+let restDisplay = `00:10`;
+let prepTimeinSec;
+let workTimeinSec;
+let restTimeinSec;
+//Event Listeners
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  roundsDisplay.textContent = `${currentRound}/${rounds.value}`;
+  whichInterval = intervalWhenPaused || "prepare";
+  prepDisplay = `${prepareMin.value}:${prepareSec.value}`;
+  workDisplay = `${workMin.value}:${workSec.value}`;
+  restDisplay = `${restMin.value}:${restSec.value}`;
+  prepTimeinSec = convertHour_Min_Sec_toSec(prepDisplay);
+  workTimeinSec = convertHour_Min_Sec_toSec(workDisplay);
+  restTimeinSec = convertHour_Min_Sec_toSec(restDisplay);
+  if (paused) {
+    startBtn.textContent = "Start";
+    paused = false;
+    // updateTime(workDisplay, restDisplay, prepDisplay, prepTimeinSec, workTimeinSec, restTimeinSec);
+    intervalId = new Timer(1000, () => {
+      updateTime(workDisplay, restDisplay, prepDisplay, prepTimeinSec, workTimeinSec, restTimeinSec);
+    });
+  }
+});
+pauseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  intervalWhenPaused = whichInterval;
+  startBtn.textContent = "Resume";
+  if (!paused) {
+    paused = true;
+
+    intervalId.stop();
+  }
+});
+resetBtn.addEventListener("click", () => {
+  paused = true;
+  intervalId.stop();
+
+  mins = 0;
+  secs = 0;
+  timeDisplay.textContent = "00:00";
+});
+let workoutsArr = [];
+saveWorkoutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  workoutsArr = JSON.parse(localStorage.getItem("workouts")) || [];
+  let currentWorkout = {
+    roundsDisplay: rounds.value,
+    prepDisplay: `${prepareMin.value}:${prepareSec.value}`,
+    workDisplay: `${workMin.value}:${workSec.value}`,
+    restDisplay: `${restMin.value}:${restSec.value}`,
+  };
+
+  workoutsArr.push(currentWorkout);
+
+  // Save the updated array to local storage
+  localStorage.setItem("workouts", JSON.stringify(workoutsArr));
+
+  WorkoutsSaved = JSON.parse(localStorage.getItem("workouts"));
+
+  sidebar.innerHTML = "";
+  showSavedWorkouts();
+});
+
+//
+
+let sec = "0" + 0;
+let min = "0" + 0;
+let time = `00:00`;
+let currentRound = 0;
+function updateTime(workTime, restTime, prepTime, prepTimeinSec, workTimeinSec, restTimeinSec) {
+  if (currentRound < rounds.value) {
+    if (whichInterval == "prepare") {
+      progressBar.value = sec;
+      progressBar.max = prepTimeinSec;
+      progressBar.style.setProperty("--progressBar-color", `#21365c`);
+    }
+    if (whichInterval == "prepare" && time > prepTime) {
+      whichInterval = "work";
+      sec = "0" + 0;
+      min = "0" + 0;
+      time = `00:00`;
+      progressBar.value = sec;
+    }
+    if (whichInterval == "work") {
+      progressBar.value = sec;
+      progressBar.max = workTimeinSec;
+      progressBar.style.setProperty("--progressBar-color", `${workColor.value}`);
+    }
+    if (whichInterval == "rest") {
+      progressBar.value = sec;
+      progressBar.max = restTimeinSec;
+      progressBar.style.setProperty("--progressBar-color", `${restColor.value}`);
+    }
+    if (whichInterval == "work" && time > workTime) {
+      whichInterval = "rest";
+      sec = "0" + 0;
+      min = "0" + 0;
+      time = `00:00`;
+      progressBar.value = sec;
+    } else if (whichInterval == "rest" && time > restTime) {
+      currentRound++;
+      roundsDisplay.textContent = `${currentRound}/${rounds.value}`;
+      whichInterval = "work";
+      sec = "0" + 0;
+      min = "0" + 0;
+      time = `00:00`;
+      progressBar.value = sec;
+    }
+    timeDisplay.textContent = `${min}:${sec}`;
+    sec++;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    if (sec == 60) {
+      min++;
+      min = min < 10 ? "0" + min : min;
+      sec = "0" + 0;
+    }
+    time = `${min}:${sec}`;
+  } else {
+    timeDisplay.textContent = `00:00`;
+
+    intervalId.stop();
+  }
+}
+function pad(unit) {
+  return ("0" + unit).length > 2 ? unit : "0" + unit;
+}
+
+function convertHour_Min_Sec_toSec(str) {
+  // let arr = str.split(":");
+  // let seconds = Number(arr[0] * 60 + +arr[1]);
+  let [minute, second] = str.split(":");
+
+  minute = parseInt(minute, 10);
+  second = parseInt(second, 10);
+  let totalSeconds = minute * 60 + second;
+  return totalSeconds;
+  // return seconds;
 }
